e27f131ee1dfeac5aacd37e464bd23490b42204f
MAP-CAD sync <sync@local>
2026-09-18 15:25:24 +0800

    发版 v2.30 2026-09-18 15:25

--- 编排器/app.py
+++ 编排器/app.py
@@ -35,7 +35,7 @@ except Exception:                    # 模块缺失时不阻塞主程序
     _lr_lbd_boxes = None
 
 APP_TITLE = "Voltage-CAD MAP"
-APP_VERSION = "2.20.1"
+APP_VERSION = "2.30"
 UPDATE_REPO = "cszmw2k6dk-design/CAD-MAP"
 UPDATE_ASSET = "Voltage-CAD MAP.exe"
 UPDATE_API = "https://api.github.com/repos/%s/releases/latest" % UPDATE_REPO
